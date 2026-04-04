fdb03a352b41093b33547d3464e373e69efaab95
Akash <avivishwakarma7yt@gmail.com>
2026-04-04 21:51:04 +0530

    implement Subract function with error handling for negative numbers

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function multiply(a,b){
     return a*b;
 }
 multiply(4,5);
+
+// Subract function
+function Subract(a,b){
+    // condition 
+    if (a < 0 || b < 0 ) {
+        throw new Error("negative number not valid");
+    }
+    return a-b;
+}
+Subract(4,5);
+Subract(3,6);
+Subract(-3,5);
